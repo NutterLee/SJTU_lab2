31541e95d28bfe5e212a1818da6d1cafd59e0c21
Handong-Lee <1737078184@qq.com>
2017-04-08 21:27:39 +0800

    按钮确定
    总体界面确定，不做%运算和平方按钮

--- lab2/lab2/mainWindow.cpp
+++ lab2/lab2/mainWindow.cpp
@@ -5,50 +5,60 @@
 #include"Point.h"
 #include"token.h"
 #include"Window.h"
-
 using namespace Graph_lib;
-/*
-class Window :public Fl_Window
-{
-public:
-	Window(int w, int h, const string& title);
-	Window(Point xy, int w, int h, const string&title);
 
-	virtual ~Window(){}
-
-	int x_max() const { return w; }
-	int y_max() const { return h; }
-
-	void resize(int ww, int hh) { w = ww, h = hh; size(ww, hh); }
-
-	void set_label(const string&) { shapes.push_back(&s); }
-	void attach(Widget&);
-
-	void detach(Shape& s);
-	void detach(Widget& w);
-
-	void put_on_top(Shape& p);
-
-protected:
-	void draw();
-
-private:
-	vector<Shape*>shapes;
-	int w, h;
-
-	void init();
-};
-*/
 struct mainWindow:Window
 {
 	mainWindow(Point xy, int w, int h, const string& title);
 private:
 
-	//计算
-	Button calculate;
-	//输入框与输出框
-	In_box input;
-	Out_box output;
+	//计算、绘图,为了简化问题，将绘画与计算分离
+	Button numCalculate;
+	Button numDraw;
 
+	//输入框与输出框
+	In_box inputBox;
+	Out_box outputBox;
+
+	//数字按钮
+	Button numZero;
+	Button numOne;
+	Button numTwo;
+	Button numThree;
+	Button numFour;
+	Button numFive;
+	Button numSix;
+	Button numSeven;
+	Button numEight;
+	Button numNine;
+	
+	//小数点
+	Button numPoint;
+
+	//()按钮
+	Button leftBracket;
+	Button rightBracket;
+
+	//运算符
+	Button multiply;
+	Button divide;
+	Button add;
+	Button decrease;
+	Button factorial;
+
+	//M R标记
+	Button numM;
+	Button numR;
+
+	//删除按钮
+	Button numDelete;
+
+	//基础坐标系
+	Axis baseAxis;
+
+	//回调函数
+	static void cb_calculate(Address, Address);//计算结果
+	static void cb_draw(Address, Address);//绘图
+	static void cb_show(Address, Address);//每输入一个字符要在输入框显示
 
 };
